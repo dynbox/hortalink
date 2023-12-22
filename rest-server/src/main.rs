39167ac434eb67f890287e9148f54e9184db5f14
Luís <baskerbyte@gmail.com>
2023-12-22 19:33:31 -0300

    feat: initialize http connection

--- rest-server/src/main.rs
+++ rest-server/src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+use axum::Router;
+use core::database::SqlxManager;
+use common::config::Settings;
+
+#[tokio::main]
+async fn main() {
+    let settings = Settings::new("application.toml");
+    let database = SqlxManager::new(&settings.webserver.database)
+        .await;
+
+    let router = Router::new()
+        .with_state(database.pool);
+
+    let listener = tokio::net::TcpListener::bind(settings.webserver.url())
+        .await
+        .unwrap();
+    axum::serve(listener, router)
+        .await
+        .expect("Failed to start axum server");
+}
