7ac9ffdf5649367e3b739f64aa291ea49fbac3d3
deltaly23 <alysontxr@gmail.com>
2024-01-05 07:46:20 -0300

    feat: update logscreen

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,68 +1,8 @@
 
-<style>
-    *{
-        box-sizing: border-box;
-        margin: 0;
-        padding: 0;
-    }
-    main{
-        position: absolute;
-        left: 0;
-        top: 0;
-        width: 100vw; 
-        height: 100vh;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-    form{
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        max-width: 425px;
-        padding: 1rem;
-    }
-    form > input{
-        width: 100%;
-        margin: 0.5rem 0.5rem 0 0.5rem;
-        padding: 0.25rem;
-    }
-    .user-type-container{
-        display:flex;
-        justify-content: space-around;
-        width: 100%;
-    }
-
-    .user-type, button{
-        margin: 0.5rem 0.5rem 0 0.5rem;
-        padding: 0.25rem;
-        border-radius: 0.25rem;
-        transition: 1s;
-    }
-    .user-active{
-        background-color: #228b22;
-        color: #fff;
-    }
-
-</style>
+<script>
+    import Logscreen from "./Logscreen.svelte"
+</script>
 
 <main>
-    <h2>bem vindo ao:</h2>
-    <h1>hortalink</h1>
-
-    <form action="">
-        <p>entre agora!</p>
-        <input type="email" placeholder="e-mail:" />
-        <input type="password" placeholder="senha:" />
-        <div class="user-type-container">
-            <span class="user-type user-active">cliente</span>
-            <span class="user-type">vendedor</span>
-        </div>
-
-        <button type="submit">enviar</button>
-    </form>
-    <p>não tem uma conta? <a href="">inscreva-se aqui</a></p>
+    <Logscreen />
 </main>
---
+++ web/src/routes/Signup.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+    import { fade } from 'svelte/transition';
+
+
+    const dispatch = createEventDispatcher();
+
+    function goBack(component: string) {
+        dispatch('goback', {
+            component: component
+        })
+    }
+</script>
+
+
+<main transition:fade={{ delay: 250, duration: 300 }}>
+    <div class="top-bar">
+        <button on:click={()=>{goBack('logscreen')}}>voltar</button>
+    </div>
+    
+    <form action="">
+        <input type="email" placeholder="e-mail:" />
+        <input type="number" placeholder="numero de telefone:" />
+        <input type="password" placeholder="senha:" />
+        <label for="account-type">tipo de conta:</label>
+        <div class="account-type">
+
+            <label for="cliente">
+                <input type="radio" name="account-type" id="cliente" />
+                cliente
+            </label>
+
+            <label for="vendedor">
+                <input type="radio" name="account-type" id="vendedor" />
+                vendedor
+            </label>
+        </div>
+        <button type="submit">inscrever-se</button>
+    </form>
+</main>
+
+
+<style>
+    .account-type{
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    .account-type label{
+        width: 50%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        transition: 150ms;
+    }
+</style>
+
+
