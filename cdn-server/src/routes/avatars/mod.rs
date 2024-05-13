8d3c0c383efc3a54c86d5e3a629f7041a6bf4ada
Luis <baskerbyte@gmail.com>
2024-05-13 17:30:00 -0400

    fix: remove unsend import

--- cdn-server/src/routes/avatars/mod.rs
+++ cdn-server/src/routes/avatars/mod.rs
@@ -1,15 +1,14 @@
 use axum::Router;
-use axum::routing::{delete, get, post};
+use axum::routing::{delete, get};
 use axum_login::login_required;
+
 use crate::app::auth::AuthGate;
 
-mod post;
 mod get;
 mod delete;
 
 pub fn router() -> Router {
     Router::new()
-        .route("/:user_id", post(post::user_avatar))
         .route("/:user_id/:hash", delete(delete::user_avatar))
         .route_layer(login_required!(AuthGate))
         .route("/:user_id/:hash", get(get::user_avatar))
