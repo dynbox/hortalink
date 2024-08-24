02ffaef966f5c4a1945f0cd9483094cacfabd967
Luis <baskerbyte@gmail.com>
2024-08-24 06:40:07 -0400

    fix: add env variable to categories

--- rest-server/src/app/server.rs
+++ rest-server/src/app/server.rs
@@ -107,6 +107,6 @@ impl Server {
             .with_secure(false)
             .with_name("session_id")
             .with_expiry(Expiry::OnInactivity(Duration::days(20)))
-            .with_domain(state.settings.web.client.host.clone())
+            .with_domain(state.settings.web.client.proxy.clone())
     }
 }
