d964451daa3c83b24a31a53f5dc310cea496d6c3
Luis <baskerbyte@gmail.com>
2024-09-07 07:19:30 -0400

    feat: use query builder to avoid sql injection

--- rest-server/src/api/v1/products/get.rs
+++ rest-server/src/api/v1/products/get.rs
@@ -1,7 +1,7 @@
 use axum::{Extension, Json};
 use axum::extract::Query;
 use axum_garde::WithValidation;
-
+use sqlx::QueryBuilder;
 use crate::app::server::AppState;
 use crate::json::error::ApiError;
 use crate::json::products::{FilterProducts, ProductDistanceQuery};
@@ -12,7 +12,8 @@ pub async fn filter_products(
     WithValidation(query): WithValidation<Query<FilterProducts>>,
 ) -> Result<Json<Vec<SellerProductPreview>>, ApiError> {
     let query = query.into_inner();
-    let mut sql_query = String::from(
+
+    let mut sql_query = QueryBuilder::new(
         r#"
             SELECT DISTINCT ON (s.id) s.id, p.id AS product_id, p.name,
                s.photos, s.price, s.unit,
@@ -22,54 +23,72 @@ pub async fn filter_products(
     );
 
     if let (Some(latitude), Some(longitude)) = (query.latitude, query.longitude) {
-        sql_query.push_str(&format!(", ST_DistanceSphere(sc.geolocation, ST_SetSRID(ST_MakePoint({longitude}, {latitude}),4674)) / 1000 AS dist"));
+        sql_query.push(", ST_DistanceSphere(sc.geolocation, ST_SetSRID(ST_MakePoint(")
+            .push_bind(longitude)
+            .push(", ")
+            .push_bind(latitude)
+            .push("),4674)) / 1000 AS dist ");
     } else {
-        sql_query.push_str(", null AS dist");
+        sql_query.push(", null AS dist");
     }
 
-    sql_query.push_str(" FROM seller_products s ");
-    
+    sql_query.push(" FROM seller_products s ");
+
     if let Some(product_id) = query.product_id {
-        sql_query.push_str(&format!("JOIN products p ON p.id = {product_id} "))
+        sql_query.push(" JOIN products p ON p.id = ")
+            .push_bind(product_id);
     } else {
-        sql_query.push_str("JOIN products p ON s.product_id = p.id ")
+        sql_query.push(" JOIN products p ON s.product_id = p.id ");
     }
-    
-    sql_query.push_str("JOIN products_schedules ps ON ps.seller_product_id = s.id JOIN schedules sc ON sc.id = ps.schedule_id ");
+
+    sql_query.push(" JOIN products_schedules ps ON ps.seller_product_id = s.id JOIN schedules sc ON sc.id = ps.schedule_id ");
 
     if let Some(day_of_week) = query.day_of_week {
-        sql_query.push_str(&format!("AND sc.day_of_week = {} ", day_of_week as i16));
+        sql_query.push(" AND sc.day_of_week = ")
+            .push_bind(day_of_week as i16);
     }
 
-    sql_query.push_str("WHERE 1 = 1 ");
+    sql_query.push(" WHERE 1 = 1 ");
 
     if let Some(max_price) = query.max_price {
-        sql_query.push_str(&format!("AND s.price < {max_price} "));
+        sql_query.push(" AND s.price < ")
+            .push_bind(max_price);
     }
 
     if let Some(min_price) = query.min_price {
-        sql_query.push_str(&format!("AND s.price > {min_price} "));
+        sql_query.push(" AND s.price > ")
+            .push_bind(min_price);
     }
 
     if let Some(min_stars) = query.min_stars {
-        sql_query.push_str(&format!("AND rating >= {} ", min_stars as i16));
+        sql_query.push(" AND COALESCE(CAST(s.rating_sum AS FLOAT) / CAST(NULLIF(s.rating_quantity, 0) AS FLOAT), NULL) >= ")
+            .push_bind(min_stars as i16);
     }
 
     if let Some(product_type) = query.product_type {
-        sql_query.push_str(&format!("AND p.category = {product_type} "));
+        sql_query.push(" AND p.category = ")
+            .push_bind(product_type);
     }
 
     if let Some(start_time) = query.start_time {
-        sql_query.push_str(&format!("AND sc.start_time > {start_time} "));
+        sql_query.push(" AND sc.start_time > ")
+            .push_bind(start_time);
     }
 
     if let (Some(latitude), Some(longitude)) = (query.latitude, query.longitude) {
-        sql_query.push_str(&format!("AND ST_DWithin(sc.geolocation, ST_SetSRID(ST_MakePoint({longitude}, {latitude}), 4674), 45000) "));
+        sql_query.push(" AND ST_DWithin(sc.geolocation::geography, ST_SetSRID(ST_MakePoint(")
+            .push_bind(longitude)
+            .push(", ")
+            .push_bind(latitude)
+            .push(")::geography, 4674), 45000) ");
     }
 
-    sql_query.push_str(&format!("ORDER BY s.id LIMIT {} OFFSET {}", query.per_page, (query.page - 1) * query.per_page));
+    sql_query.push(" ORDER BY s.id LIMIT ")
+        .push_bind(query.per_page)
+        .push(" OFFSET ")
+        .push_bind((query.page - 1) * query.per_page);
 
-    let products = sqlx::query_as::<_, SellerProductPreview>(&sql_query)
+    let products = sql_query.build_query_as::<SellerProductPreview>()
         .fetch_all(&state.pool)
         .await?;
 
@@ -95,6 +114,6 @@ pub async fn distance(
         .bind(query.products_id)
         .fetch_all(&state.pool)
         .await?;
-    
+
     Ok(Json(products))
 }
